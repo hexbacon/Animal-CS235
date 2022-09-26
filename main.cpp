6068ed38e7b733622756332eb9fc0810a488a774
Christopher Mena <mena.christopher.student@gmail.com>
2022-09-25 20:18:59 -0400

    Working LinkedBag

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include "Animal.h"
 #include "Mammal.h"
+#include "LinkedBag.h"
 int main() {
     Animal lion;
+    Animal cat_2;
     Mammal cat("Cat", true, true);
+    LinkedBag<Animal> a_bag;
     cat.setFins();
     cat.setTail();
     cat.setLegs(4);
-
     lion.setName("Lion");
-    std::cout << lion.getName() << std::endl;
-    std::cout << cat.getName() << ", legs: " << cat.legs() << std::endl;
+    cat_2 = cat;
+    a_bag.add(lion);
+    a_bag.add(cat);
+    std::cout << "Items count in bag: " << a_bag.getCurrentSize() << std::endl;
+    std::cout << "Cat 2: " << cat_2.getName() << std::endl;
     return 0;
 }
